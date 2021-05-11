8f956e59d77b5e228f1138e38c15b173fa3e2f05
gvozdik2014@gmail.com <gvozdik2014@gmail.com>
2021-05-11 20:19:02 +0300

    add types

--- model.ts
+++ model.ts
@@ -1,3 +1,66 @@
+export type { Types } from "./interface";
+import cuid from "cuid";
+
 export class Model {
-  constructor() {}
+  private types: Types[];
+  constructor() {
+    this.types = [
+      {
+        id: cuid(),
+        name: "books",
+        items: [
+          {
+            // typeName: "books",
+            id: cuid(),
+            name: "Java for child",
+            details: [{ description: "about all", autor: "12334" }],
+          },
+          { id: cuid(), name: "Java for child 2" },
+        ],
+      },
+      {
+        id: cuid(),
+        name: "movies",
+        items: [
+          {
+            id: cuid(),
+            name: "Alone home",
+            details: [{ description: "about all", year: 2015 }],
+          },
+        ],
+      },
+      {
+        id: cuid(),
+        name: "tools",
+        items: [
+          {
+            id: cuid(),
+            name: "hammer",
+            details: [{ long: "10cm", weight: "1kg" }],
+          },
+        ],
+      },
+    ];
+  }
+  getAllTypes(): Types[] {
+    return this.types;
+  }
+  getItemByType(id) {
+    let itemByType = null;
+    this.types.forEach((item) => {
+      if (item.id == id) {
+        itemByType = item;
+      }
+    });
+    return itemByType;
+  }
+  getDetailByItem(id) {
+    let itemByType = null;
+    this.types.forEach((item) => {
+      if ((item.items.id = id)) {
+        itemByType = item;
+      }
+    });
+    return itemByType;
+  }
 }
